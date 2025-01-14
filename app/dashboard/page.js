28c86d7b8aa3d8f26ca43e5156662b77ec26fdfd
Md Rizwan <rizwandev99@gmail.com>
2025-01-14 17:10:37 +0530

    Refactor Home page to use Dashboard component and clean up globals.css by removing unused styles

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,12 +1,13 @@
+import Dashboard from "@/components/Dashboard";
 import Login from "@/components/Login";
 import Main from "@/components/Main";
 import React from "react";
 
-export default function Dashboard() {
+export default function DashboardPage() {
   const isAuthenticated = false;
 
   let children = isAuthenticated ? <Dashboard /> : <Login />;
 
-  return (<Main>{children}</Main>);
+  return <Main>{children}</Main>;
 }
-// 
+//
